a7d5f4636add09e2d1d832e2da8210e3d7fcc9b6
b19karhj <sc2rena@gmail.com>
2023-03-24 18:20:08 +0100

    Regex now works in php
    Now how regex and the two search query working. Only setting up ajax and implement PDO left. Then pilot study is ready

--- Backend.php
+++ Backend.php
@@ -1,26 +1,43 @@
 <?php
-   require_once __DIR__ . '/vendor/autoload.php';
-
+use MongoDB\Tests\SpecTests\ResultExpectation;
+   require __DIR__ . '/vendor/autoload.php';
    
    // connect to mongodb
    $client = new MongoDB\Client("mongodb://localhost:27017");
 	//$database = $client->database;
+   //$collection = $database->collection;
    $collection = $client->Examen2023->Examen;
   
+   
+   //$query1 = $collection->find([ 'Password' => [ '$regex' => '%love$' ]]);
 
+   //$cursor = $collection->findOne(['Password' => "iloveyou"]); // Returns object whith password equals "iloveyou"
 
-   //$cursor = $collection->findOne(['Password' => 'iloveyou']); // Returns object whith password equals "iloveyou"
-   $cursor = $collection->find(['Password' => 'rock']);
-   var_dump($cursor);
+   //var_dump($query1);
+
+   //$cursor = $collection->find(['Password' => [$regex ], 'rock']);
+  // var_dump($cursor);
    /*foreach($client->listDatabases() as $db){ // Listar alla databaser som finns.
       var_dump($db); 
    };*/
+// Set up query with regex
+   $filter = ['Password' => ['$regex' => 'love']]; // Find documents where 'name' field starts with 'J'
+   $options = [];
 
-  /* foreach($database->listCollections() as $collection){
-      var_dump($collection);
-   }*/
+   // Execute query
+ 
+   $cursor =  $collection->find($filter);
    
-   //Todo. query ett password och få hits som response.
+   $dataArray = $cursor->toArray();
+   $theHash = $_POST['hashString'];
+   print($theHash); 
+
+  /*foreach ($dataArray as $document){ 
+      //$hits = $document->Hits; $password = $document->Password; 
+      print($hits ."\n"); 
+   }
+   return // skicka tillbaka.
+   //Todo. query ett password och få hits som response.*/
 
  
 
