74ba8b1f54e1e13569c877b27375d700f414e6ca
b19karhj <sc2rena@gmail.com>
2023-03-30 01:05:40 +0200

    Comment in php and loop prep for ajax
    Added loop in index where a word length of less or equal too 2 will be sent in as space

--- Backend.php
+++ Backend.php
@@ -1,18 +1,36 @@
 <?php
-   require __DIR__ . '/vendor/autoload.php';
+   require __DIR__ . '/vendor/autoload.php'; //BEHOVER TAS BORT FOR RIKTIGA SERVERN
    //Connection
    $client = new MongoDB\Client("mongodb://localhost:27017");
    $collection = $client->Examen2023->Examen;
-   //$arrayTest = array("iloveyou12345678","iloveyou123456789","iloveyou123456");
+   //$arrayTest = array("iloveyou12345678","iloveyou123456789","iloveyou123456"); //Test array
    if(isset($_POST['noSegPassword'],$_POST['segPassword'])){
       $password = $_POST['noSegPassword'];
-      $segmentedPassword = $_POST['segPassword'];
-      
-      foreach($segmentedPassword as $seg){
-         echo $seg;
-      }
-   }else{
-      echo 'NotNull'; //returnera ett stop tillbaka till javascript ajax.
-   }
+      $segmentedPassword = $_POST['segPassword']->toArray();
+      $cursor = $collection->findOne(['Password' => $password]); // Returns single object if found
 
+      if($cursor == null){
+         $responseArray = [];
+         for ($i = 0; $i < count($responseArray); $i++){
+            $totalHits = 0;
+            $filter = ['Password' => ['$regex' => $responseArray[$i]]]; // Find documents with regex include
+            $cursor =  $collection->find($filter);
+            $dataArray = $cursor->toArray();
+
+            foreach ($dataArray as $document){ 
+               $hits = $document->Hits;  //Saves the number from Hits
+               $totalHits += $hits;  //Adds and saves the total value of hits
+            }
+
+            $responseArray[] = $totalHits; // Saves the total hits in array
+         };
+
+         foreach($responseArray as $result){ //Test print for Array
+            print($result."\n");
+         };
+         
+      }
+}else{
+   echo 'NotNull'; //returnera ett stop tillbaka till javascript ajax.
+}
 ?>
