10d825e7ab025d7071bbc6f68e60385f316f317e
b19karhj <sc2rena@gmail.com>
2023-03-29 23:56:08 +0200

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,25 +1,24 @@
 import json
-from pymongo import MongoClient
+import pymongo
 
-myclient = MongoClient("mongodb://localhost:27017")
+
+myclient = pymongo.MongoClient("mongodb+srv://Rena:@examen2023.rho8i7v.mongodb.net/test")
 
 db = myclient["Examen2023"] #Databasen
 
-
 Collection = db["Examen"] #Collection
-
-
-
 
 # Inserting the loaded data in the Collection
 with open('C:/Users/Sc2re/OneDrive/Dokument/Examen/Db/passwords.json') as file:
     file_data = json.load(file)
-    
+    print("test")
 for obj in file_data.values():
+    print("for")
     try:
         Collection.insert_one(obj)
         print(obj)
-    except Exception:
+    except Exception as e:
+        print(e)
         pass
 
 print("done")
