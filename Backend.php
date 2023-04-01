1e5e4e68fd1f53d3eb5f31403d46d2b3c9125078
b19karhj <sc2rena@gmail.com>
2023-04-01 17:10:54 +0200

    Ajax now working
    Ajax and php Backend now working as intended

--- Backend.php
+++ Backend.php
@@ -1,36 +1,33 @@
 <?php
-   require __DIR__ . '/vendor/autoload.php'; //BEHOVER TAS BORT FOR RIKTIGA SERVERN
-   //Connection
-   $client = new MongoDB\Client("mongodb://localhost:27017");
-   $collection = $client->Examen2023->Examen;
-   //$arrayTest = array("iloveyou12345678","iloveyou123456789","iloveyou123456"); //Test array
-   if(isset($_POST['noSegPassword'],$_POST['segPassword'])){
-      $password = $_POST['noSegPassword'];
-      $segmentedPassword = $_POST['segPassword']->toArray();
-      $cursor = $collection->findOne(['Password' => $password]); // Returns single object if found
-
-      if($cursor == null){
-         $responseArray = [];
-         for ($i = 0; $i < count($responseArray); $i++){
-            $totalHits = 0;
-            $filter = ['Password' => ['$regex' => $responseArray[$i]]]; // Find documents with regex include
-            $cursor =  $collection->find($filter);
-            $dataArray = $cursor->toArray();
-
-            foreach ($dataArray as $document){ 
-               $hits = $document->Hits;  //Saves the number from Hits
-               $totalHits += $hits;  //Adds and saves the total value of hits
-            }
-
-            $responseArray[] = $totalHits; // Saves the total hits in array
-         };
-
-         foreach($responseArray as $result){ //Test print for Array
-            print($result."\n");
-         };
+      require __DIR__ . '/vendor/autoload.php';
+      //Connection
+      $client = new MongoDB\Client("mongodb+srv://Rena:ZynMonster%40!23@examen2023.rho8i7v.mongodb.net/test");
+      $collection = $client->Examen2023->Examen;
+      
+      if(isset($_POST['segPassword']) && isset($_POST['noSegPassword'])){
+         $password = json_decode($_POST['noSegPassword']);
+         $segmentedPassword = json_decode($_POST['segPassword'], true);
+         $cursor = $collection->findOne(['Password' => $password]); // Returns single object if found
          
-      }
-}else{
-   echo 'NotNull'; //returnera ett stop tillbaka till javascript ajax.
-}
+         if($cursor == null){
+            $responseArray = [];
+            for ($i = 0; $i < count($segmentedPassword); $i++){
+               $totalHits = 0;
+               $filter = ['Password' => ['$regex' => $segmentedPassword[$i]]]; // Find documents with regex include
+               $cursor =  $collection->find($filter);
+               $dataArray = $cursor->toArray();
+   
+               foreach ($dataArray as $document){ 
+                  $hits = $document->Hits;  //Saves the number from Hits
+                  $totalHits += $hits;  //Adds and saves the total value of hits
+               }
+   
+               $responseArray[] = $totalHits; // Saves the total hits in array
+            };
+            echo json_encode($responseArray);
+            
+         }else if ($cursor != null){
+            echo json_encode(true); //returnera ett stop tillbaka till javascript ajax.
+         }
+   }
 ?>
